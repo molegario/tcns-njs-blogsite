8aed5d162c2e0c3b3d8cc13596b5cdf8ce462305
molegario <molegario@x2omedia.com>
2025-02-07 16:19:38 -0500

    implemented role check for admin via user meta data in clerk.com service

--- app/(dashboard)/(routes)/allposts/page.tsx
+++ app/(dashboard)/(routes)/allposts/page.tsx
@@ -4,10 +4,20 @@
 // import Link from "next/link";
 // import NavbarMain from "../_components/navbar";
 
+// import {
+//   Protect
+// } from '@clerk/nextjs';
 
+import { checkRole } from '@/utils/roles';
 const AllPosts = async function () {
+  const isAdmin = await checkRole('admin');
   return (
     <>
+      {
+        isAdmin && (
+          <p>Admin is logged in.</p>
+        )
+      }
       <div className="h-[640px] w-full flex flex-col align-middle shadow-sm justify-center">
         ALL POSTS
       </div>
