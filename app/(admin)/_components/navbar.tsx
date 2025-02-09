bf9d687a5afe485ec2d5a3a68ec5fa0f6301da54
molegario <molegario@x2omedia.com>
2025-02-09 13:22:20 -0500

    added access to create post on all posts page

--- app/(admin)/_components/navbar.tsx
+++ app/(admin)/_components/navbar.tsx
@@ -2,7 +2,7 @@ import { NavbarRoutes } from "@/components/navbar-routes";
 import { MobileSidebar } from "./mobile-sidebar";
 import { checkRole } from "@/lib/roles";
 
-export const Navbar = async function () {
+export const Navbar = async () => {
   const isAdmin = await checkRole("admin");
 
   return (
--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,11 +1,14 @@
 import { NavbarRoutes } from "@/components/navbar-routes";
 import { MobileSidebar } from "./mobile-sidebar";
+import { checkRole } from "@/lib/roles";
 
-export const Navbar = () => {
+export const Navbar = async () => {
+  const isAdmin = await checkRole("admin");
+
   return (
     <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
       <MobileSidebar />
-      <NavbarRoutes />
+      <NavbarRoutes isAdmin={isAdmin}/>
     </div>
   );
 };
