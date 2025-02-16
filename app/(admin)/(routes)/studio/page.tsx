1dace4c042895d47506482ac4ab8b07b6f25adf3
molegario <molegario@x2omedia.com>
2025-02-16 08:42:07 -0500

    code cleanup. started layout of studio home. installed table components

--- app/(admin)/(routes)/studio/page.tsx
+++ app/(admin)/(routes)/studio/page.tsx
@@ -1,4 +1,6 @@
+import { IconBadge } from "@/components/icon-badge";
 import { checkRole } from "@/lib/roles";
+import { BookHeart, ListChecks } from "lucide-react";
 import { redirect } from "next/navigation";
 
 const StudioPage = async function () {
@@ -10,8 +12,30 @@ const StudioPage = async function () {
 
   return (
     <>
-      <div className="h-[640px] w-full flex flex-col align-middle shadow-sm justify-center">
-        Studio PAGE
+      <div className="p-6">
+        <div className="flex items-center justify-between">
+          <div className="w-full">
+            <div className="flex items-center justify-between w-full">
+              <div className="flex flex-col gap-y-2">
+                <h1 className="text-2xl font-medium">Creator Studio</h1>
+              </div>
+            </div>
+            <div className="grid grid-col-1 md:grid-cols-6 gap-6 mt-16">
+              <div className="md:col-span-2">
+                <div className="flex items-center gap-x-2">
+                  <IconBadge icon={ListChecks} />
+                  <h3 className="text-xl">Content Statistics</h3>
+                </div>
+              </div>
+              <div className="md:col-span-4">
+                <div className="flex items-center gap-x-2">
+                  <IconBadge icon={BookHeart} />
+                  <h3 className="text-xl">Content Management</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
